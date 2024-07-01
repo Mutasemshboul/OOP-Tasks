349e251c714b64cee7ad7a9986442c5e1983fc0f
Mutasem <mutasemshboul14@gmail.com>
2024-07-01 21:01:08 +0300

    Add project files.

---
+++ OOP - Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP___Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
